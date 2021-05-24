550534623bb7fe54e77ae357c68e5464814cb82f
MasterD98 <dasithrathnasinghe@gmail.com>
2021-05-24 09:51:55 +0530

    preview news

--- Scraper.py
+++ Scraper.py
@@ -29,5 +29,5 @@ def create_custom_hn():
       if points > 99:
         hn.append({'title': title, 'link': href, 'votes': points})
   return sort_stories_by_votes(hn)
- 
+
 pprint.pprint(create_custom_hn())
--- Server.py
+++ Server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
+import Scraper
 app=Flask(__name__)
 print(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    news=Scraper.create_custom_hn()
+    return render_template('index.html', news=news  )
